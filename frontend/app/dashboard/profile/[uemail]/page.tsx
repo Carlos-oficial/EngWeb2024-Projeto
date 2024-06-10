adc563dd9cd1ff986f9b544b17c1da9a1c23b4e5
Júlio Pinto <pinto.julioj03@gmail.com>
2024-06-10 02:37:09 +0100

    profile first try

--- frontend/app/dashboard/profile/[uemail]/page.tsx
+++ frontend/app/dashboard/profile/[uemail]/page.tsx
@@ -5,6 +5,7 @@ import { ResourceDTO, UserDTO } from '@/lib/types';
 import { useEffect, useState } from 'react';
 import { useSession } from 'next-auth/react';
 import ResourceCard from '@/components/resource_card';
+import { Avatar, AvatarFallback, AvatarImage } from '@radix-ui/react-avatar';
 
 export default function Profile({ params }: { params: { uemail: string } }) {
   const [resources, setResources] = useState<ResourceDTO[] | null>(null);
@@ -29,14 +30,38 @@ export default function Profile({ params }: { params: { uemail: string } }) {
   return (
     <div>
       <div>
-        <p>User: {params.uemail}</p>
-        <p>me: {JSON.stringify(userData)}</p>
         <p></p>
-        {resources
-          ? resources.map((resource) => (
-              <ResourceCard resource={resource} key={resource._id} />
-            ))
-          : 'Loading...'}
+        <div className='p-5 space-y-3 overflow-scroll w-full'>
+          <div className="mx-16 my-4 mb-8">
+            <div className='flex flew-col gap-3"'>
+              <div className='basis-2/3 rounded-full overflow-hidden'>
+                <Avatar>
+                  <AvatarImage src={userData?.image} />
+                </Avatar>
+              </div>
+              <div className='mx-8 my-4'>
+                <p className='text-5xl font-semibold'>{userData?.name}</p>
+                <p className='text-lg font-normal mt-2'>{userData?.email}</p>
+                <p className='text-md font-normal mt-2'>
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet tellus nunc. Suspendisse potenti. Suspendisse eget aliquam quam. Sed viverra justo pharetra arcu auctor rutrum. Donec porta nibh ac mi pulvinar aliquet. Pellentesque vestibulum libero id enim lobortis viverra nec in tortor. Nulla ultricies ut dui eu posuere. Morbi eu quam nec velit fringilla finibus id vitae nisi. Fusce interdum a purus id lacinia. Nulla laoreet diam ut tincidunt tincidunt. Duis aliquet, quam ac accumsan pharetra, felis eros mollis nunc, vitae consectetur dolor nisi nec metus. Etiam leo purus, semper et aliquam a, sodales id enim. </p>
+              </div>
+            </div>
+          </div>
+          <hr/>
+          <div>
+            <p className='text-2xl font-bold my-4'>Resources</p>
+            <div className='grid gap-3 md:grid-cols-2 lg:grid-cols-3'>
+            {resources
+              ? resources.map((resource) => (
+                  <ResourceCard resource={resource} key={resource._id} />
+                ))
+              : 'Loading...'}
+            </div>
+          </div>
+          
+        </div>
+        
+        
       </div>
     </div>
   );
