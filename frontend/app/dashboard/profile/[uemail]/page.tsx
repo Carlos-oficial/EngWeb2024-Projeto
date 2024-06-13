f86a88f2ec0cf66764825e87a7a886a219a4bc72
Júlio Pinto <pinto.julioj03@gmail.com>
2024-06-13 01:24:40 +0100

    last touches

--- frontend/app/dashboard/profile/[uemail]/page.tsx
+++ frontend/app/dashboard/profile/[uemail]/page.tsx
@@ -11,55 +11,58 @@ import ProfileEditDialog from '@/components/profile_dialog';
 
 export default function Profile({ params }: { params: { uemail: string } }) {
   const [resources, setResources] = useState<ResourceDTO[] | null>(null);
+  const [userData, setUserData] = useState<UserDTO | null>(null);
   const [error, setError] = useState<string>('');
   const session = useSession();
 
+  const decodeEmail = decodeURIComponent(params.uemail);
+
   useEffect(() => {
-    if (session.status === 'authenticated')
-      listResourcesByUser(session.data?.user?.email ?? '')
+    if (params.uemail) {
+      listResourcesByUser(decodeEmail)
         .then((resources) => setResources(resources))
         .catch((error: Error) => setError(error.message));
-  }, [session]);
-
-  const [userData, setUserData] = useState<UserDTO | null>(null);
+    }
+  }, [params.uemail]);
 
   useEffect(() => {
-    getUser(session.data?.user?.email ?? '')
-      .then((userData) => setUserData(userData))
-      .catch((error: Error) => setError(error.message));
-  }, [session]);
+    if (params.uemail) {
+      getUser(decodeEmail)
+        .then((userData) => setUserData(userData))
+        .catch((error: Error) => setError(error.message));
+    }
+  }, [params.uemail]);
 
   return (
     <div className='flex h-full w-full'>
       <div className='p-5 space-y-3 overflow-scroll w-full'>
         <div className='px-5 space-y-3 overflow-scroll w-full py-2 border-border'>
-            <p className='text-2xl font-bold my-4'>Resources</p>
-            <div className='grid gap-3 md:grid-cols-2 lg:grid-cols-3'>
+          <p className='text-2xl font-bold my-4'>Resources</p>
+          <div className='grid gap-3 md:grid-cols-2 lg:grid-cols-3'>
             {resources
               ? resources.map((resource) => (
                   <ResourceCard resource={resource} key={resource._id} />
                 ))
               : 'Loading...'}
-            </div>
+          </div>
         </div>
       </div>
       <div className='p-5 space-y-3 overflow-scroll w-1/3 h-full border-l border-border'>
-          <div className="mx-16 my-4 mb-8">
-              <div className='rounded-full overflow-hidden'>
-                <Avatar>
-                  <AvatarImage src={userData?.image} />
-                </Avatar>
-              </div>
+        <div className='mx-16 my-4 mb-8'>
+          <div className='rounded-full overflow-hidden'>
+            <Avatar>
+              <AvatarImage src={userData?.image} />
+            </Avatar>
+          </div>
 
-              <p className='text-5xl font-semibold text-center mt-8'>{userData?.name}</p>
-              <p className='text-lg font-normal text-center mt-2'>{userData?.email}</p>
-              {
-                session.data?.user.email === decodeURIComponent(params.uemail) && (
-                  <ProfileEditDialog></ProfileEditDialog>                  
-                )
-              }
-          </div>          
+          <p className='text-5xl font-semibold text-center mt-8'>{userData?.name}</p>
+          <p className='text-lg font-normal text-center mt-2'>{userData?.email}</p>
+          {session.data?.user.email === decodeURIComponent(params.uemail) && (
+            <ProfileEditDialog />
+          )}
+          {error && <p className='text-red-500 text-center mt-4'>{error}</p>}
         </div>
+      </div>
     </div>
   );
 }
