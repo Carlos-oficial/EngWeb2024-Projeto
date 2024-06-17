873937257eef75501d2136df027e0c99785185a2
Diogo Matos <diogomatosemail@gmail.com>
2024-06-17 23:35:11 +0100

    fix: typo

--- scripts/import.sh
+++ scripts/import.sh
@@ -4,7 +4,7 @@ if [ -z "$1" ] || [ $1 = "--help" ] || [ $1 = "-h" ]; then
 fi
 
 # Define the MongoDB database name
-db_name=`cat $1/db`
+db_name=`cat $1/db_name`
 echo db name is $db_name
 db_folder=$1/$db_name
 echo db folder is $db_folder
@@ -16,4 +16,4 @@ for file in $db_folder/*.json; do
     mongoimport --uri "mongodb://localhost:27017/$db_name" --collection $collection_name --file $file
 done
 
-unzip -d  public/uploads $1/uploads.zip 
+unzip -d public/uploads $1/uploads.zip
